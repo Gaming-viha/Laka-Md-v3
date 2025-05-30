c012fa80998a34f59367c504860fe04cfc1581e9
Gaming-viha <gamingvihangasathsara12345@gmail.com>
2025-05-30 13:30:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Session id",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=k14HBLgZ#zNnJVESVpWwsAFdNzp_a42FUx2p9RLzc4JiaCC0y5X0",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
